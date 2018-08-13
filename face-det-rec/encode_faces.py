316e0b8b45ca4f7652225d2629180eb46c219089
goruck <lindo.st.angel@gmail.com>
2018-08-12 18:17:46 -0700

    inital face det rec code

---
+++ face-det-rec/encode_faces.py
@@ -0,0 +1,85 @@
+# USAGE
+# python encode_faces.py --dataset dataset --encodings encodings.pickle
+
+# import the necessary packages
+from imutils import paths
+import face_recognition
+import argparse
+import pickle
+import cv2
+import os
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--dataset", required=True,
+	help="path to input directory of faces + images")
+ap.add_argument("-e", "--encodings", required=True,
+	help="path to serialized db of facial encodings")
+ap.add_argument("-d", "--detection-method", type=str, default="cnn",
+	help="face detection model to use: either `hog` or `cnn`")
+args = vars(ap.parse_args())
+
+# grab the paths to the input images in our dataset
+print("[INFO] quantifying faces...")
+imagePaths = list(paths.list_images(args["dataset"]))
+
+# initialize the list of known encodings and known names
+knownEncodings = []
+knownNames = []
+
+# loop over the image paths
+for (i, imagePath) in enumerate(imagePaths):
+	# extract the person name from the image path
+	print("[INFO] processing image {}/{}".format(i + 1,
+		len(imagePaths)))
+	name = imagePath.split(os.path.sep)[-2]
+
+	# load the input image and convert it from RGB (OpenCV ordering)
+	# to dlib ordering (RGB)
+	image = cv2.imread(imagePath)
+
+	# resize image if W > 800 or H > 600
+	IMG_W_MAX = 800
+	IMG_H_MAX = 600
+	(img_h, img_w) = image.shape[0:2]
+	print('image {}'.format(imagePath))
+	print ('width = {} height = {}'.format(img_w, img_h))
+	if img_w > IMG_W_MAX:
+		r = IMG_W_MAX / image.shape[1]
+		dim = (IMG_W_MAX, int(image.shape[0] * r))
+		resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+		rgb = cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)
+		(img_h, img_w) = resized.shape[0:2]
+		print ('resized -> width = {} height = {}'.format(img_w, img_h))
+	elif img_h > IMG_H_MAX:
+		r = IMG_H_MAX / image.shape[0]
+		dim = (int(image.shape[1] * r), IMG_H_MAX)
+		resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+		rgb = cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)
+		(img_h, img_w) = resized.shape[0:2]
+		print ('resized -> width = {} height = {}'.format(img_w, img_h))
+	else:
+		rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+	# detect the (x, y)-coordinates of the bounding boxes
+	# corresponding to each face in the input image
+	# added number_of_times_to_upsample=0 since default crashes system
+	boxes = face_recognition.face_locations(rgb, number_of_times_to_upsample=1,
+		model=args["detection_method"])
+
+	# compute the facial embedding for the face
+	encodings = face_recognition.face_encodings(rgb, boxes)
+
+	# loop over the encodings
+	for encoding in encodings:
+		# add each encoding + name to our set of known names and
+		# encodings
+		knownEncodings.append(encoding)
+		knownNames.append(name)
+
+# dump the facial encodings + names to disk
+print("[INFO] serializing encodings...")
+data = {"encodings": knownEncodings, "names": knownNames}
+f = open(args["encodings"], "wb")
+f.write(pickle.dumps(data))
+f.close()
---
+++ face-det-rec/face_det_rec.py
@@ -0,0 +1,108 @@
+#
+# Face detection and recognition using dlib and the face_recognition api.
+# 
+# References:
+# See https://github.com/ageitgey/face_recognition.
+# See https://www.pyimagesearch.com/2018/06/18/face-recognition-with-opencv-python-and-deep-learning/.
+#
+# Part of the smart-zoneminder project:
+# See https://github.com/goruck/smart-zoneminder.
+#
+# Copyright (c) 2018 Lindo St. Angel.
+#
+
+import face_recognition
+import argparse
+import pickle
+import cv2
+import json
+
+import sys
+from sys import argv
+from sys import exit
+from sys import stdout
+
+# Get image paths from command line.
+if len(sys.argv) == 1:
+    exit('No test image file paths were supplied!')
+
+# Construct list from images given on command line. 
+objects_detected = argv[1:]
+
+# load the known faces and embeddings
+# TODO - add error handleing. 
+data = pickle.loads(open('/home/lindo/develop/smart-zoneminder/face-det-rec/encodings.pickle', "rb").read())
+
+# List that will hold all images with any face detection information. 
+objects_detected_faces = []
+
+# Loop over the images given in the command line.  
+for obj in objects_detected:
+	json_obj = json.loads(obj)
+	img = cv2.imread(json_obj['image'])
+
+	# initialize the list of names for each face detected
+	names = []
+	for label in json_obj['labels']:
+		if label['name'] == 'person':
+			# First bound the roi using the coord info passed in.
+			# The roi is area around person(s) detected in image.
+
+			# (x1, y1) are the top left roi coordinates.
+			# (x2, y2) are the bottom right roi coordinates.
+			y2 = int(label['box']['ymin'])
+			x1 = int(label['box']['xmin'])
+			y1 = int(label['box']['ymax'])
+			x2 = int(label['box']['xmax'])
+
+			#cv2.rectangle(img, (x1, y1), (x2, y2), (255,0,0), 2)
+			roi = img[y2:y1, x1:x2, :]
+
+			# Covert from cv2 format.
+			rgb = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
+
+			# detect the (x, y)-coordinates of the bounding boxes corresponding
+			# to each face in the input image, then compute the facial embeddings
+			# for each face
+			box = face_recognition.face_locations(rgb, number_of_times_to_upsample=1, model='cnn')
+
+			encodings = face_recognition.face_encodings(rgb, box)
+
+			# loop over the facial embeddings
+			for encoding in encodings:
+				# attempt to match each face in the input image to our known encodings
+				matches = face_recognition.compare_faces(data["encodings"],
+					encoding)
+				name = "Unknown"
+
+				# check to see if we have found a match
+				if True in matches:
+					# find the indexes of all matched faces then initialize a
+					# dictionary to count the total number of times each face
+					# was matched
+					matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+					counts = {}
+
+					# loop over the matched indexes and maintain a count for
+					# each recognized face face
+					for i in matchedIdxs:
+						name = data["names"][i]
+						counts[name] = counts.get(name, 0) + 1
+
+					# determine the recognized face with the largest number of
+					# votes (note: in the event of an unlikely tie Python will
+					# select first entry in the dictionary)
+					name = max(counts, key=counts.get)
+
+				# update the list of names
+				names.append(name)
+
+			# Add face label.
+			label['faces'] = names
+
+	# Add processed image to output list. 
+	objects_detected_faces.append(json_obj)
+
+# Convert json to string and return data. 
+print(json.dumps(objects_detected_faces))
+sys.stdout.flush()
