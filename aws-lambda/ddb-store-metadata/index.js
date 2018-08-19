8a2bceba791b7d04cb61e696447a5716148ae344
goruck <lindo.st.angel@gmail.com>
2018-08-19 08:14:20 -0700

    removed old code

--- aws-lambda/ddb-store-metadata/index.js
+++ aws-lambda/ddb-store-metadata/index.js
@@ -1,5 +1,4 @@
 exports.handler = (event, context, callback) => {
-
     //
     // Writes image metadata to DynamoDB.
     // Normally the last processing step. 
@@ -9,12 +8,9 @@ exports.handler = (event, context, callback) => {
 
     // Create the DynamoDB service object
     const documentClient = new AWS.DynamoDB.DocumentClient(
-                               {apiVersion: '2012-10-08',
-                               region: process.env.AWS_REGION});
+        {apiVersion: '2012-10-08', region: process.env.AWS_REGION});
     
     // Retrieve parameters from export handler event
-    //var bucket = event.bucket;
-    //var filename = event.key;
     const S3Key = event.newFilename;
     const S3DateTime = event.newFilenameDate;
     const alertState = event.Alert;
@@ -25,55 +21,6 @@ exports.handler = (event, context, callback) => {
     const zmEventId = event.metadata.zmeventid;
     const zmFrameId = event.metadata.zmframeid;
     const zmScore = event.metadata.zmscore;
-    
-    /*try {
-        // Make an array of strings from S3Key...
-        var split1 = S3Key.split('/');
-        // 1st element contains 'archive' (not used).
-        // 2nd element contains 'alerts' or 'falsepositives' (not used).
-        // 3rd element contains camera name.
-        var zmCameraName = split1[2];
-        // 4th element contains event date.
-        var eventDateStr = split1[3];
-        // Then parse into items for ddb.
-        var eventDateArr = eventDateStr.split('-');
-        var zmEventYear  = eventDateArr[0];
-        var zmEventMonth = eventDateArr[1];
-        var zmEventDay   = eventDateArr[2];
-        // 5th element contains event hour (not used).
-        // 6th element contains event, frame and time info of alarm.
-        var alarmInfoStr = split1[5];
-        // Then parse into items for ddb.
-        var alarmInfoArr = alarmInfoStr.split('-');
-        var zmEventName  = alarmInfoArr[0];
-        var zmEventId    = alarmInfoArr[1].replace(/\D/g, '');
-        var zmFrameId    = alarmInfoArr[2].replace(/\D/g, '');
-        var zmEventHour  = alarmInfoArr[3];
-        var zmEventMin   = alarmInfoArr[4];
-        var zmEventSec   = alarmInfoArr[5];
-        var zmEventmSec  = alarmInfoArr[6].replace('.jpg', '');
-    }
-    catch(err) {
-        var errorMessage =  'Error in [ddb-store-metadata S3Key parse].\r' + 
-                            '   Function input ['+JSON.stringify(event, null, 2)+'].\r' +  
-                            '   Error ['+err+'].';
-        console.log(errorMessage);
-        callback(err, null);
-    }*/
-    
-    /*var tempDateTime = new Date(zmEventYear, (zmEventMonth - 1), zmEventDay,
-                                zmEventHour, zmEventMin, zmEventSec, zmEventmSec);*/
-                                
-    //console.log('temp ts: '+tempDateTime);
-                        
-    // tzOffset = 8 * 60 * 60 * 1000.
-    // Assumes daylight savings time.
-    // TODO: make this conversion more robust.
-    //var tzOffset = 28800000;
-    
-    //var zmEventDateTime = new Date(tempDateTime.getTime() + tzOffset);
-                                    
-    //console.log('TS: '+zmEventDateTime);
     
     // Parameters for DynamoDB.
     const params = {
