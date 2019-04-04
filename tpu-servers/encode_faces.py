2d6db29c46a9efa337c63e4bee39f309e3ccd981
goruck <lindo.st.angel@gmail.com>
2019-04-04 16:59:55 -0700

    added encode_face.py and train.py

---
+++ tpu-servers/encode_faces.py
@@ -0,0 +1,112 @@
+# USAGE
+# python encode_faces.py --dataset dataset --encodings encodings.pickle
+
+from imutils import paths
+import face_recognition
+import argparse
+import pickle
+import cv2
+import os
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--dataset", required=True,
+	help="path to input directory of faces + images")
+ap.add_argument("-e", "--encodings", required=True,
+	help="path to serialized db of facial encodings")
+ap.add_argument("-d", "--detection-method", type=str, default="cnn",
+	help="face detection model to use: either `hog` or `cnn`")
+args = vars(ap.parse_args())
+
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+	# ref: https://stackoverflow.com/questions/44650888/resize-an-image-without-distortion-opencv
+
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    # return the resized image
+    return resized
+
+# grab the paths to the input images in our dataset
+print("[INFO] quantifying faces...")
+imagePaths = list(paths.list_images(args["dataset"]))
+
+# initialize the list of known encodings and known names
+knownEncodings = []
+knownNames = []
+
+# loop over the image paths
+for (i, imagePath) in enumerate(imagePaths):
+	# extract the person name from the image path
+	print("[INFO] processing image {}/{}".format(i + 1,
+		len(imagePaths)))
+	name = imagePath.split(os.path.sep)[-2]
+
+	# load the input image
+	image = cv2.imread(imagePath)
+
+	# resize and convert it from BGR (OpenCV ordering)
+	# to dlib ordering (RGB)
+	resized = image_resize(image, width = 600)
+	rgb = cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)
+
+	# detect the (x, y)-coordinates of the bounding boxes
+	# corresponding to each face in the input image
+	# Do not increase upsample beyond 1 else you'll run out of memory.
+	boxes = face_recognition.face_locations(rgb, number_of_times_to_upsample=1,
+		model=args["detection_method"])
+
+	if len(boxes) == 0:
+	 print('*** no face found! ***')
+	 continue
+
+	# compute the facial embedding for the face
+	embedder = cv2.dnn.readNetFromTorch('./nn4.v2.t7')
+	# carve out face ROI from image
+    # then construct a blob for the face ROI, then pass the blob
+    # through our face embedding model to obtain the 128-d
+    # quantification of the face 
+	face_top, face_right, face_bottom, face_left = boxes[0]
+	face_roi = resized[face_top:face_bottom, face_left:face_right, :]
+	faceBlob = cv2.dnn.blobFromImage(face_roi, 1.0 / 255, (96, 96), (0, 0, 0), swapRB=True, crop=False)
+	embedder.setInput(faceBlob)
+	encodings = embedder.forward()
+	print(encodings)
+
+	# loop over the encodings
+	for encoding in encodings:
+		# add each encoding + name to our set of known names and
+		# encodings
+		knownEncodings.append(encoding)
+		knownNames.append(name)
+
+# dump the facial encodings + names to disk
+print("[INFO] serializing encodings...")
+data = {"encodings": knownEncodings, "names": knownNames}
+f = open(args["encodings"], "wb")
+f.write(pickle.dumps(data))
+f.close()
