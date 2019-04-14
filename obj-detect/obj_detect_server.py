f3f2215b4396411928bb13a6f95d3782c00e75bc
goruck <lindo.st.angel@gmail.com>
2019-04-14 09:39:38 -0700

    graceful shutdown & code cleanup

--- obj-detect/obj_detect_server.py
+++ obj-detect/obj_detect_server.py
@@ -20,6 +20,8 @@ import cv2
 import json
 import zerorpc
 import logging
+import gevent
+import signal
 # Object detection imports.
 from object_detection.utils import label_map_util
 # For tensorrt optimized models...
@@ -57,7 +59,12 @@ ZRPC_HEARTBEAT = config['zerorpcHeartBeat']
 # This must match the zerorpc client config.
 ZRPC_PIPE = config['zerorpcPipe']
 
-# Load frozen Tensorflow model into memory. 
+# Only grow the gpu memory tf usage as required.
+# See https://www.tensorflow.org/guide/using_gpu#allowing-gpu-memory-growth
+config = tf.ConfigProto()
+config.gpu_options.allow_growth=True
+
+# Load frozen tf model into memory.
 detection_graph = tf.Graph()
 with detection_graph.as_default():
     od_graph_def = tf.GraphDef()
@@ -65,12 +72,6 @@ with detection_graph.as_default():
         serialized_graph = fid.read()
         od_graph_def.ParseFromString(serialized_graph)
         tf.import_graph_def(od_graph_def, name='')
-    # Only grow the memory usage as required.
-    # See https://www.tensorflow.org/guide/using_gpu#allowing-gpu-memory-growth
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    sess = tf.Session(config=config, graph=detection_graph)
-    sess.run(tf.global_variables_initializer())
 
 # Load tf label map. 
 label_map = label_map_util.load_labelmap(PATH_TO_LABEL_MAP)
@@ -78,97 +79,130 @@ categories = label_map_util.convert_label_map_to_categories(label_map,
     max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
-# zerorpc server.
-class DetectRPC(object):
-    def detect_objects(self, test_image_paths):
-       with detection_graph.as_default():
-            objects_in_image = []
-            old_labels = []
-            frame_num = 0
-            monitor = ''
-            (img_width, img_height) = (CROP_IMAGE_WIDTH, CROP_IMAGE_HEIGHT)
-            for image_path in test_image_paths:
-                logging.info('**********Find object(s) for {}'.format(image_path))
-                # If consecutive frames then repeat last label to minimize processing.
-                # Image paths must be in the form of:
-                # '/nvr/zoneminder/events/BackPorch/18/06/20/19/20/04/00224-capture.jpg'.
-                old_frame_num = frame_num
-                old_monitor = monitor
-                try:
-                    frame_num = int((image_path.split('/')[-1]).split('-')[0])
-                    monitor = image_path.split('/')[4]
-                except (ValueError, IndexError):
-                    logging.error("Could not derive information from image path.")
-                    objects_in_image.append({'image': image_path, 'labels': []})
-                    continue
+def skip_inference(frame_num, monitor, labels, image_path, objects_in_image):
+    """
+    If consecutive frames then repeat last label and skip a new inference.
+    
+    Image paths must be in the form of:
+    '/nvr/zoneminder/events/BackPorch/18/06/20/19/20/04/00224-capture.jpg'.
+    """
+    old_frame_num = frame_num
+    old_monitor = monitor
+    skip = False
+    try:
+        frame_num = int((image_path.split('/')[-1]).split('-')[0])
+        monitor = image_path.split('/')[4]
+    except (ValueError, IndexError):
+        logging.error("Could not derive information from image path.")
+        objects_in_image.append({'image': image_path, 'labels': []})
+        skip = True
+        return skip, frame_num, monitor
                     
-                # Only apply skip logic if frames are from the same monitor. 
-                if monitor == old_monitor:
-                    # Only apply skip logic if alarm frames are from the same event.
-                    # Intra-event frames are monotonically increasing.
-                    frame_diff = frame_num - old_frame_num
-                    if frame_diff > 0:
-                        # Skip CON_IMG_SKIP frames after the first one. 
-                        if frame_diff  <= CON_IMG_SKIP:
-                            objects_in_image.append({'image': image_path, 'labels': old_labels})
-                            logging.debug('monitor {} old_monitor {} frame_num {} old_frame_num {}'
-                                .format(monitor,old_monitor,frame_num,old_frame_num))
-                            logging.info('Consecutive frame {}, skipping detect and copying previous labels.'
-                                .format(frame_num))
-                            continue
-
-                # Read image from disk. 
-                img = cv2.imread(image_path)
-                #cv2.imwrite('./img.jpg', img)
-                if img is None:
-                    # Bad image was read.
-                    logging.error('Bad image was read.')
-                    objects_in_image.append({'image': image_path, 'labels': []})
-                    continue
-
-                # Resize to minimize tf processing.
-                # Note: resize will slightly lower accuracy. 640 x 480 seems like a good balance.
-                res = cv2.resize(img, dsize=(img_width, img_height), interpolation=cv2.INTER_AREA)
-                #cv2.imwrite('./res.jpg', res)
-                # Format np array for tf use. 
-                image_tf = res.astype(np.uint8)
-                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-                image_tf_expanded = np.expand_dims(image_tf, axis=0)
-                # Define input node.
-                image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-                # Define output nodes.
-                # Each box represents a part of the image where a particular object was detected.
-                boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-                # This contains class scores for the detections.
-                scores = detection_graph.get_tensor_by_name('detection_scores:0')
-                # This contains classes for the detections.
-                classes = detection_graph.get_tensor_by_name('detection_classes:0')
-                # This specifies the number of valid boxes per image in the batch.
-                num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-
-                # Actual detection.
-                (boxes, scores, classes, num_detections) = sess.run(
-                    [boxes, scores, classes, num_detections],
-                    feed_dict={image_tensor: image_tf_expanded})
-
-                # Get labels and scores of detected objects.
-                labels = []
-                #(w, h) = image.size # use original image size for box coords
-                (h, w) = img.shape[:2] # use original image size for box coords
-                for index, value in enumerate(classes[0]):
-                    if scores[0, index] > MIN_SCORE_THRESH:
-                        object_dict = {}
-                        object_dict['id'] = category_index.get(value)['id']
-                        object_dict['name'] = category_index.get(value)['name']
-                        object_dict['score'] = float(scores[0, index])
-                        (ymin, xmin, ymax, xmax) = boxes[0, index] * np.array([h, w, h, w])
-                        object_dict['box'] = {'ymin': ymin, 'xmin': xmin, 'ymax': ymax, 'xmax': xmax}
-                        labels.append(object_dict)
-
-                old_labels = labels
+    # Only apply skip logic if frames are from the same monitor. 
+    if monitor == old_monitor:
+        # Only apply skip logic if alarm frames are from the same event.
+        # Intra-event frames are monotonically increasing.
+        frame_diff = frame_num - old_frame_num
+        if frame_diff > 0:
+            # Skip CON_IMG_SKIP frames after the first one. 
+            if frame_diff <= CON_IMG_SKIP:
                 objects_in_image.append({'image': image_path, 'labels': labels})
-            return json.dumps(objects_in_image)
+                logging.debug('monitor {} old_monitor {} frame_num {} old_frame_num {}'
+                    .format(monitor,old_monitor,frame_num,old_frame_num))
+                logging.debug('Consecutive frame {}, skipping detect and copying previous labels.'
+                    .format(frame_num))
+                skip = True
+                        
+    return skip, frame_num, monitor
 
-s = zerorpc.Server(DetectRPC(), heartbeat=ZRPC_HEARTBEAT)
+# zerorpc class.
+class DetectRPC(object):
+    def __init__(self):
+        logging.debug('Starting tf sess.')
+        self.sess = tf.Session(config=config, graph=detection_graph)
+
+    def close_sess(self):
+        logging.debug('Closing tf sess.')
+        self.sess.close()
+
+    def detect_objects(self, test_image_paths):
+        objects_in_image = [] # holds all objects found in image
+        labels = [] # labels of detected objects
+        frame_num = 0 # ZoneMinder current alarm frame number
+        monitor = '' # ZoneMinder current monitor name
+        (img_width, img_height) = (CROP_IMAGE_WIDTH, CROP_IMAGE_HEIGHT)
+
+        for image_path in test_image_paths:
+            logging.debug('**********Find object(s) for {}'.format(image_path))
+
+            # If consecutive frames then repeat last label and skip inference.
+            # This behavior controlled by CON_IMG_SKIP.
+            skip, frame_num, monitor = skip_inference(frame_num, monitor,
+                labels, image_path, objects_in_image)
+            if skip is True:
+                continue
+
+            # Read image from disk. 
+            img = cv2.imread(image_path)
+            #cv2.imwrite('./img.jpg', img)
+            if img is None:
+                # Bad image was read.
+                logging.error('Bad image was read.')
+                objects_in_image.append({'image': image_path, 'labels': []})
+                continue
+
+            # Resize to minimize tf processing.
+            # Note: resize will slightly lower accuracy. 640 x 480 seems like a good balance.
+            res = cv2.resize(img, dsize=(img_width, img_height), interpolation=cv2.INTER_AREA)
+            #cv2.imwrite('./res.jpg', res)
+            # Format np array for tf use. 
+            image_tf = res.astype(np.uint8)
+            # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+            image_tf_expanded = np.expand_dims(image_tf, axis=0)
+            # Define input node.
+            image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+            # Define output nodes.
+            # Each box represents a part of the image where a particular object was detected.
+            boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+            # This contains class scores for the detections.
+            scores = detection_graph.get_tensor_by_name('detection_scores:0')
+            # This contains classes for the detections.
+            classes = detection_graph.get_tensor_by_name('detection_classes:0')
+            # This specifies the number of valid boxes per image in the batch.
+            num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+            # Actual detection.
+            (boxes, scores, classes, num_detections) = self.sess.run(
+                [boxes, scores, classes, num_detections],
+                feed_dict={image_tensor: image_tf_expanded})
+
+            # Get labels and scores of detected objects.
+            labels = [] # new detection, clear labels list.
+            (h, w) = img.shape[:2] # use original image size for box coords
+            for index, value in enumerate(classes[0]):
+                if scores[0, index] > MIN_SCORE_THRESH:
+                    object_dict = {}
+                    object_dict['id'] = category_index.get(value)['id']
+                    object_dict['name'] = category_index.get(value)['name']
+                    object_dict['score'] = float(scores[0, index])
+                    (ymin, xmin, ymax, xmax) = boxes[0, index] * np.array([h, w, h, w])
+                    object_dict['box'] = {'ymin': ymin, 'xmin': xmin, 'ymax': ymax, 'xmax': xmax}
+                    labels.append(object_dict)
+
+            objects_in_image.append({'image': image_path, 'labels': labels})
+
+        return json.dumps(objects_in_image)
+
+# Create zerorpc object. 
+zerorpc_obj = DetectRPC()
+# Create and bind zerorpc server. 
+s = zerorpc.Server(zerorpc_obj, heartbeat=ZRPC_HEARTBEAT)
 s.bind(ZRPC_PIPE)
+# Register graceful ways to stop server. 
+gevent.signal(signal.SIGINT, s.stop) # Ctrl-C
+gevent.signal(signal.SIGTERM, s.stop) # termination
+# Start server.
+# This will block until a gevent signal is caught
 s.run()
+# After server is stopped then close the tf session. 
+zerorpc_obj.close_sess()
