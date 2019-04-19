cb0f01d80e9ce7d0fd1977fe9ba3d2c93e8ed932
goruck <lindo.st.angel@gmail.com>
2019-04-18 20:18:14 -0700

    added resize fn and changed logging

--- face-det-rec/face_detect_server.py
+++ face-det-rec/face_detect_server.py
@@ -73,10 +73,10 @@ def svm_face_classifier(encoding, min_proba):
 	logging.debug('svm proba {} name {}'.format(proba, le.classes_[j]))
 	if proba >= min_proba:
 		name = le.classes_[j]
-		logging.info('svm says this is {}'.format(name))
+		logging.debug('svm says this is {}'.format(name))
 	else:
 		name = None # prob too low to recog face
-		logging.info('svm cannot recognize face')
+		logging.debug('svm cannot recognize face')
 	return name
 
 def variance_of_laplacian(image):
@@ -84,7 +84,40 @@ def variance_of_laplacian(image):
 	# measure, which is simply the variance of the Laplacian
 	return cv2.Laplacian(image, cv2.CV_64F).var()
 
-# zerorpc server.
+def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    # ref: https://stackoverflow.com/questions/44650888/resize-an-image-without-distortion-opencv
+
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation=inter)
+
+    # return the resized image
+    return resized
+
+# Define zerorpc class.
 class DetectRPC(object):
     def detect_faces(self, test_image_paths):
         # List that will hold all images with any face detection information. 
@@ -92,7 +125,7 @@ class DetectRPC(object):
 
         # Loop over the images paths provided. 
         for obj in test_image_paths:
-            logging.info('**********Find Face(s) for {}'.format(obj['image']))
+            logging.debug('**********Find Face(s) for {}'.format(obj['image']))
             for label in obj['labels']:
                 # If the object detected is a person then try to identify face. 
                 if label['name'] == 'person':
@@ -112,7 +145,7 @@ class DetectRPC(object):
                     x1 = int(label['box']['xmin'])
                     y1 = int(label['box']['ymax'])
                     x2 = int(label['box']['xmax'])
-                    roi = img[y2:y1, x1:x2, :]
+                    roi = img[y2:y1, x1:x2]
                     #cv2.imwrite('./roi.jpg', roi)
                     if roi.size == 0:
                         # Bad object roi...move on to next image.
@@ -122,18 +155,23 @@ class DetectRPC(object):
 
                     # Detect the (x, y)-coordinates of the bounding boxes corresponding
                     # to each face in the input image.
-                    rgb = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
+                    # Resize to ensure images aren't too small or large (you'll run OOM).
+                    res = image_resize(roi, width=320)
+                    rgb = cv2.cvtColor(res, cv2.COLOR_BGR2RGB)
+                    #cv2.imwrite('./rgb.jpg', rgb)
                     detection = face_recognition.face_locations(rgb, NUMBER_OF_TIMES_TO_UPSAMPLE,
                         FACE_DET_MODEL)
                     if not detection:
                         # No face detected...move on to next image.
-                        logging.info('No face detected.')
+                        logging.debug('No face detected.')
                         label['face'] = None
                         continue
 
-                    # Carve out face roi from object roi. 
                     face_top, face_right, face_bottom, face_left = detection[0]
-                    face_roi = roi[face_top:face_bottom, face_left:face_right, :]
+                    #cv2.rectangle(rgb, (face_left, face_top), (face_right, face_bottom), (255,0,0), 2)
+                    #cv2.imwrite('./face_rgb.jpg', rgb)
+                    # Carve out face roi from resized object roi. 
+                    face_roi = res[face_top:face_bottom, face_left:face_right]
                     #cv2.imwrite('./face_roi.jpg', face_roi)
 
                     # Compute the focus measure of the face
@@ -145,7 +183,7 @@ class DetectRPC(object):
                     # If fm below a threshold then face probably isn't clear enough
                     # for face recognition to work, so skip it. 
                     if fm < FOCUS_MEASURE_THRESHOLD:
-                        logging.info('Face too blurry to recognize.')
+                        logging.debug('Face too blurry to recognize.')
                         name = None
                     else:
                         # Find the 128-dimension face encoding for face in image.
@@ -154,9 +192,6 @@ class DetectRPC(object):
                         encoding = face_recognition.face_encodings(rgb,
                             known_face_locations=[face_location], num_jitters=NUM_JITTERS)[0]
                         logging.debug('face encoding {}'.format(encoding))
-                        #cv2.rectangle(rgb, (face_left, face_top), (face_right, face_bottom), (255,0,0), 2)
-                        #cv2.imwrite('./face_rgb.jpg', rgb)
-
                         # Perform svm classification on the encodings to recognize the face.
                         name = svm_face_classifier(encoding, MIN_SVM_PROBA)
 
