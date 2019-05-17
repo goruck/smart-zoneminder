a1d2e3ec11c116371eabea78ed44e49fb8f54809
goruck <lindo.st.angel@gmail.com>
2019-05-16 19:11:48 -0700

    split train and test & add model eval

--- face-det-rec/train.py
+++ face-det-rec/train.py
@@ -10,6 +10,8 @@ Copyright (c) 2019 Lindo St. Angel
 from sklearn.preprocessing import LabelEncoder
 from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix 
 import pickle
 
 # Path to known face encodings.
@@ -22,42 +24,61 @@ SVM_LABEL_PATH = '/home/lindo/develop/smart-zoneminder/face-det-rec/face_labels.
 
 # Load the known faces and embeddings.
 with open(KNOWN_FACE_ENCODINGS_PATH, 'rb') as fp:
-	data = pickle.load(fp)
+    data_pickle = pickle.load(fp)
+
+# Encodings are stored as arrays, convert to list.
+data = [i.tolist() for i in data_pickle['encodings']]
+#print('data {}'.format(data['encodings'])
 
 # Encode the labels.
 print('Encoding labels...')
 le = LabelEncoder()
-labels = le.fit_transform(data['names'])
+labels = le.fit_transform(data_pickle['names'])
+#print('labels {}'.format(labels))
 
-def svc_param_selection(x, y, nfolds):
+def svc_param_selection(X, y, nfolds):
     # Exhaustive search over specified parameter values for svm.
-    Cs = [0.001, 0.01, 0.1, 1, 10]
-    gammas = [0.001, 0.01, 0.1, 1]
-    param_grid = {'C': Cs, 'gamma' : gammas}
-    grid_search = GridSearchCV(SVC(kernel='rbf'), param_grid, iid=False, cv=nfolds)
-    grid_search.fit(x, y)
-    grid_search.best_params_
+    Cs = [0.001, 0.01, 0.1, 1, 10, 100]
+    gammas = [0.001, 0.01, 0.1, 1, 10, 100]
+    param_grid = [
+        {'C': Cs, 'kernel': ['linear']},
+        {'C': Cs, 'gamma': gammas, 'kernel': ['rbf']}]
+    grid_search = GridSearchCV(SVC(), param_grid, iid=False, cv=nfolds)
+    grid_search.fit(X, y)
     return grid_search.best_params_
+
+# Split data up into train and test sets.
+(X_train, X_test, y_train, y_test) = train_test_split(data, labels, test_size=0.20)
+#print('X_train: {} X_test: {} y_train: {} y_test: {}'.format(X_train, X_test, y_train, y_test))
 
 # Find best parameters.
 print('Finding best parameters...')
-best = svc_param_selection(data['encodings'], labels, 15)
+best = svc_param_selection(X_train, y_train, 15)
 best_C = best['C']
-best_gamma = best['gamma']
-print('C {}, gamma {}'.format(best_C, best_gamma))
+best_gamma = best.get('gamma', 'auto_deprecated')
+best_kernel = best['kernel']
+print('kernel: {} C: {}, gamma: {}'.format(best_kernel, best_C, best_gamma))
 
 # Train the model used to accept the 128-d embeddings of the face and
 # then produce the actual face recognition model. 
 print('Training model...')
-recognizer = SVC(C=best_C, kernel='rbf', gamma=best_gamma, probability=True)
-recognizer.fit(data['encodings'], labels)
+recognizer = SVC(C=best_C, kernel=best_kernel, gamma=best_gamma, probability=True)
+recognizer.fit(X_train, y_train)
+
+# Evaluate model.
+print('Evaluating model...')
+y_pred = recognizer.predict(X_test)
+print('Confusion matrix:')
+print(confusion_matrix(y_test, y_pred))
+print('Classification matrix:')
+print(classification_report(y_test, y_pred))
 
 # Write the face recognition model to disk.
 print('Saving model...')
 with open(SVM_MODEL_PATH, 'wb') as outfile:
-	outfile.write(pickle.dumps(recognizer))
+    outfile.write(pickle.dumps(recognizer))
 
 # Write the label encoder to disk.
 print('Saving label encoder...')
 with open(SVM_LABEL_PATH, 'wb') as outfile:
-	outfile.write(pickle.dumps(le))
+    outfile.write(pickle.dumps(le))
