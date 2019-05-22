bc72ba2976d84a6f77c753ae92d20cc712b4a6f3
goruck <lindo.st.angel@gmail.com>
2019-05-21 20:03:52 -0700

    fixed whitespace

--- face-det-rec/encode_faces.py
+++ face-det-rec/encode_faces.py
@@ -12,15 +12,15 @@ import os
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--dataset", required=True,
-	help="path to input directory of faces + images")
+    help="path to input directory of faces + images")
 ap.add_argument("-e", "--encodings", required=True,
-	help="path to serialized db of facial encodings")
+    help="path to serialized db of facial encodings")
 ap.add_argument("-d", "--detection-method", type=str, default="cnn",
-	help="face detection model to use: either `hog` or `cnn`")
+    help="face detection model to use: either `hog` or `cnn`")
 args = vars(ap.parse_args())
 
 def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
-	# ref: https://stackoverflow.com/questions/44650888/resize-an-image-without-distortion-opencv
+    # ref: https://stackoverflow.com/questions/44650888/resize-an-image-without-distortion-opencv
 
     # initialize the dimensions of the image to be resized and
     # grab the image size
@@ -62,40 +62,40 @@ knownNames = []
 
 # loop over the image paths
 for (i, imagePath) in enumerate(imagePaths):
-	# extract the person name from the image path
-	print("[INFO] processing image {}/{}".format(i + 1,
-		len(imagePaths)))
-	name = imagePath.split(os.path.sep)[-2]
+    # extract the person name from the image path
+    print("[INFO] processing image {}/{}".format(i + 1,
+        len(imagePaths)))
+    name = imagePath.split(os.path.sep)[-2]
 
-	# load the input image
-	image = cv2.imread(imagePath)
+    # load the input image
+    image = cv2.imread(imagePath)
 
-	# resize image
-	# and convert it from BGR (OpenCV ordering)
-	# to dlib ordering (RGB)
-	resized = image_resize(image, width = 600)
-	rgb = cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)
+    # resize image
+    # and convert it from BGR (OpenCV ordering)
+    # to dlib ordering (RGB)
+    resized = image_resize(image, width = 600)
+    rgb = cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)
 
-	# detect the (x, y)-coordinates of the bounding boxes
-	# corresponding to each face in the input image
-	# Do not increase upsample beyond 1 else you'll run out of memory.
-	boxes = face_recognition.face_locations(rgb, number_of_times_to_upsample=1,
-		model=args["detection_method"])
+    # detect the (x, y)-coordinates of the bounding boxes
+    # corresponding to each face in the input image
+    # Do not increase upsample beyond 1 else you'll run out of memory.
+    boxes = face_recognition.face_locations(rgb, number_of_times_to_upsample=1,
+        model=args["detection_method"])
 
-	if len(boxes) == 0:
-	 print('*** no face found! ***')
-	 continue
+    if len(boxes) == 0:
+     print('*** no face found! ***')
+     continue
 
-	# compute the facial embedding for the face
-	encodings = face_recognition.face_encodings(rgb, boxes, num_jitters=500)
-	print(encodings)
+    # compute the facial embedding for the face
+    encodings = face_recognition.face_encodings(rgb, boxes, num_jitters=500)
+    print(encodings)
 
-	# loop over the encodings
-	for encoding in encodings:
-		# add each encoding + name to our set of known names and
-		# encodings
-		knownEncodings.append(encoding)
-		knownNames.append(name)
+    # loop over the encodings
+    for encoding in encodings:
+        # add each encoding + name to our set of known names and
+        # encodings
+        knownEncodings.append(encoding)
+        knownNames.append(name)
 
 # dump the facial encodings + names to disk
 print("[INFO] serializing encodings...")
