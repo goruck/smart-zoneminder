b84c119660fa1c039a0c4e669c41196d149fd187
goruck <lindo.st.angel@gmail.com>
2019-10-14 05:08:45 -0700

    added clean option

--- person-class/move-files.py
+++ person-class/move-files.py
@@ -1,14 +1,50 @@
+"""
+Copy training data from face-det-rec to person-class and split into train/val/test.
+
+Copyright (c) 2019 Lindo St. Angel
+"""
+
 import os
 import shutil
+import cv2
+import logging
 from random import shuffle
 
-def split_and_move_files(src_dir=None, tgt_dir=None):
-    if src_dir is None or tgt_dir is None:
+logging.basicConfig(level=logging.ERROR)
+
+# Minimum height and width of image to copy (in pixels).
+H_MIN = 0
+W_MIN = 0
+
+# Set True to clean up (remove) folder contents before moves.
+CLEAN = True
+
+def split_and_copy_files(src_dir=None, dst_dir=None):
+    if src_dir is None or dst_dir is None:
+        return
+
+    def copy(src_dir, dst_dir, name, dir_name):
+        src_path = src_dir + '/' + item
+        dst_path = dst_dir + '/' + dir_name +'/' + name + '/' + item
+        img = cv2.imread(src_path)
+        if img.size == 0:
+            logging.error('Error: cannot read {}'.format(src_path))
+            return
+        (h, w) = img.shape[:2]
+        if h < H_MIN or w < W_MIN:
+            return
+        if CLEAN:
+            try:
+                os.remove(dst_path)
+            except OSError as e:
+                logging.debug('Debug: {} - {}.'.format(e.filename, e.strerror))
+        logging.info('Info: moving {} to {}'.format(src_path, dst_path))
+        shutil.copy(src_path, dst_path)
         return
 
     # e.g., './dataset/lindo_st_angel/' => 'lindo_st_angel'
     name = os.path.basename(src_dir)
-    print('name {}'.format(name))
+    logging.info('Info: working on name {}'.format(name))
 
     # Get files in source dir and shuffle.
     files = os.listdir(src_dir)
@@ -26,16 +62,16 @@ def split_and_move_files(src_dir=None, tgt_dir=None):
 
     # Move files to target directories.
     for item in train:
-        shutil.copy(src_dir + '/' + item, tgt_dir + '/train/' + name + "/" + item)
+        copy(src_dir, dst_dir, name, 'train')
     for item in val:
-        shutil.copy(src_dir + '/' + item, tgt_dir + '/validation/' + name + "/" + item)
+        copy(src_dir, dst_dir, name, 'validation')
     for item in test:
-        shutil.copy(src_dir + '/' + item, tgt_dir + '/test/' + name + "/" + item)
+        copy(src_dir, dst_dir, name, 'test')
 
     return
 
-split_and_move_files('./dataset/eva_st_angel', './dataset')
-split_and_move_files('./dataset/lindo_st_angel', './dataset')
-split_and_move_files('./dataset/nico_st_angel', './dataset')
-split_and_move_files('./dataset/nikki_st_angel', './dataset')
-split_and_move_files('./dataset/Unknown', './dataset')
+split_and_copy_files('/home/lindo/develop/smart-zoneminder/face-det-rec/dataset/eva_st_angel', './dataset')
+split_and_copy_files('/home/lindo/develop/smart-zoneminder/face-det-rec/dataset/lindo_st_angel', './dataset')
+split_and_copy_files('/home/lindo/develop/smart-zoneminder/face-det-rec/dataset/nico_st_angel', './dataset')
+split_and_copy_files('/home/lindo/develop/smart-zoneminder/face-det-rec/dataset/nikki_st_angel', './dataset')
+split_and_copy_files('/home/lindo/develop/smart-zoneminder/face-det-rec/dataset/Unknown', './dataset')
