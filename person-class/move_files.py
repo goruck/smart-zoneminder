8a3626cd672f7339227f80f2f05a1c823996b915
goruck <lindo.st.angel@gmail.com>
2019-11-28 11:14:58 -0800

    added explicit test fraction + code cleanup

--- person-class/move_files.py
+++ person-class/move_files.py
@@ -6,45 +6,39 @@ Copyright (c) 2019 Lindo St. Angel
 
 import os
 import shutil
-import cv2
 import logging
 from random import shuffle
 
 logging.basicConfig(level=logging.ERROR)
 
-# Minimum height and width of image to copy (in pixels).
-H_MIN = 0
-W_MIN = 0
-
 # Set True to clean up (remove) folder contents before moves.
 CLEAN = True
+# Percentage of samples in train, validation and test sets.
+# TRAIN + VAL + TEST must equal 1.0.
+TRAIN = 0.8
+VAL = 0.2
+TEST = 0.
+
+def clean(dst_dir, name, dir_name):
+    dst_path = dst_dir + '/' + dir_name +'/' + name
+    files = os.listdir(dst_path)
+    for item in files:
+        try:
+            os.remove(dst_path + '/' + item)
+        except OSError as e:
+            logging.debug('Debug: rm error {} - {}.'.format(e.filename, e.strerror))
+    return
+
+def copy(src_dir, dst_dir, name, dir_name, samples):
+    for item in samples:
+        src_path = src_dir + '/' + item
+        dst_path = dst_dir + '/' + dir_name +'/' + name + '/' + item
+        logging.info('Info: moving {} to {}'.format(src_path, dst_path))
+        shutil.copy(src_path, dst_path)
+    return
 
 def split_and_copy_files(src_dir=None, dst_dir=None):
     if src_dir is None or dst_dir is None:
-        return
-
-    def clean(dst_dir, name, dir_name):
-        dst_path = dst_dir + '/' + dir_name +'/' + name
-        files = os.listdir(dst_path)
-        for item in files:
-            try:
-                os.remove(dst_path + '/' + item)
-            except OSError as e:
-                logging.debug('Debug: rm error {} - {}.'.format(e.filename, e.strerror))
-        return
-
-    def copy(src_dir, dst_dir, name, dir_name):
-        src_path = src_dir + '/' + item
-        dst_path = dst_dir + '/' + dir_name +'/' + name + '/' + item
-        img = cv2.imread(src_path)
-        if img.size == 0:
-            logging.error('Error: cannot read {}'.format(src_path))
-            return
-        (h, w) = img.shape[:2]
-        if h < H_MIN or w < W_MIN:
-            return
-        logging.info('Info: moving {} to {}'.format(src_path, dst_path))
-        shutil.copy(src_path, dst_path)
         return
 
     # e.g., './dataset/lindo_st_angel/' => 'lindo_st_angel'
@@ -56,13 +50,13 @@ def split_and_copy_files(src_dir=None, dst_dir=None):
     shuffle(files)
 
     # Split files into train, validation and test sets.
-    # Default is 80% of data is in train set and 10% in validation and test.
     num_samples = len(files)
-    num_train_samples = int(0.8*num_samples)
-    num_val_samples = int(0.1*num_samples)
+    num_train_samples = int(TRAIN*num_samples)
+    num_val_samples = int(VAL*num_samples) if TEST else num_samples - num_train_samples
     #num_test_samples = num_samples - num_train_samples - num_val_samples
     train = files[:num_train_samples]
     val = files[num_train_samples:(num_train_samples + num_val_samples)]
+    # Samples not taken by test and val sets (if any) will be placed in a test set.
     test = files[(num_train_samples + num_val_samples):]
 
     # Remove all files in target directories if CLEAN True.
@@ -72,12 +66,9 @@ def split_and_copy_files(src_dir=None, dst_dir=None):
         clean(dst_dir, name, 'test')
 
     # Move files to target directories.
-    for item in train:
-        copy(src_dir, dst_dir, name, 'train')
-    for item in val:
-        copy(src_dir, dst_dir, name, 'validation')
-    for item in test:
-        copy(src_dir, dst_dir, name, 'test')
+    copy(src_dir, dst_dir, name, 'train', train)
+    copy(src_dir, dst_dir, name, 'validation', val)
+    copy(src_dir, dst_dir, name, 'test', test)
 
     return
 
